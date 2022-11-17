1c37c5ea65478e7f1df75a6d18c977744214065d
Alberto Barrago <a.barrago69@gmail.com>
2022-11-17 22:24:47 +0100

    :lips: Added info for funny

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <div className="grid grid-cols-1 gap-1 text-center">
-            <h1 className="text-center mt-10 text-5xl">Hello!</h1>
-            <p className='mt-3'>Work in progress...</p>
+        <div className="grid grid-cols-2 gap-1 text-center">
+            <div className="mt-[10%] flex flex-col items-center">
+                <Image src='/images/troll-pilled.gif'
+                       height={600}
+                       width={600}
+                       alt='troll'>
+                </Image>
+            </div>
+            <div>
+                <Typography variant="h1" className="mt-52 text-3xl">
+                    Ciao stiamo sperimentando giusto due
+                    cosette... <br/> Volete contribuire? <br/>
+                    Non potete 😂 <br/> <br/> Scherzo!
+                    per ulteriori info  <br/> scrivetemi una  <a href="mailto:alberto.barrago@gmail.com&subject?subject=Richiesta info!&body=Ciao Alberto, ">email</a> </Typography>
+            </div>
         </div>
     )
 }
